4a444e1ffe0155366e2e0f34fc43a65300293c98
pandeyak1991 <64955950+pandeyak1991@users.noreply.github.com>
2023-06-02 17:51:15 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,10 +1,11 @@
 pipeline{
 agent any
-   stages{
-       stage("hello"){
-                  steps{         echo "hello"                
-                       }}
-           }           
+  stages{
+  stage("echo"){
+    steps{
+     echo "Hello world"
+    }
+  }       
 }
 
 
