7fef3922a31a57b88f0bd4826b6df457260256a2
pandeyak1991 <64955950+pandeyak1991@users.noreply.github.com>
2023-06-02 17:27:17 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,8 +1,9 @@
 pipelineJob('example') {
-    definition {
-        cps {
-            script(readFileFromWorkspace('project-a-workflow.groovy'))
-            sandbox()
+           stringParam {
+            name('FolderName')
+            defaultValue('Avhana-Prod')
+            description('''The name of the folder, where jobs will be created - this is based on the Release environment name
+eg: KIBO-master, KIBO-Release etc.''')
+            trim(false)
         }
-    }
 }
