5a23fc1df83d35c1583d31289d96b92eccd0fa7c
pandeyak1991 <64955950+pandeyak1991@users.noreply.github.com>
2023-06-02 17:47:28 +0530

    Update pipeline.groovy

--- pipeline.groovy
+++ pipeline.groovy
@@ -1,9 +1,10 @@
-pipelineJob('example') {
-           stringParam {
-            name('FolderName')
-            defaultValue('Avhana-Prod')
-            description('''The name of the folder, where jobs will be created - this is based on the Release environment name
-eg: KIBO-master, KIBO-Release etc.''')
-            trim(false)
-        }
+pipeline{
+agent any
+           stages{
+                      stage("hello"){
+                            echo "hello"                
+                      }
+           }           
 }
+
+
